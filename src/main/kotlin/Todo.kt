0638c292814035fa4f9a1add99df97576b54c23f
Emerick Bosch <xpc.dev@gmail.com>
2021-09-29 15:27:31 +0200

    Make a call against the Miro API

--- src/main/kotlin/Todo.kt
+++ src/main/kotlin/Todo.kt
@@ -1,9 +1,13 @@
 import io.ktor.client.*;
 import io.ktor.client.engine.apache.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.observer.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import kotlinx.coroutines.*
+import kotlinx.serialization.*
 import java.io.FileInputStream
 import java.util.*
 
@@ -15,7 +19,7 @@ fun main() = runBlocking {
     val miroProperties = getMiroProperties();
     println(miroProperties);
 
-    if(miroProperties != null) {
+    if (miroProperties != null) {
         callMiro(miroProperties)
     }
 }
@@ -26,7 +30,7 @@ fun main() = runBlocking {
 fun getMiroProperties(): MiroProperties? {
     val miroPropertiesFile = "miro.properties"
     val propertiesPath = object {}.javaClass.getResource(miroPropertiesFile)?.path;
-    if(propertiesPath == null) {
+    if (propertiesPath == null) {
         println("Unable to find: $miroPropertiesFile")
         return null;
     }
@@ -36,7 +40,7 @@ fun getMiroProperties(): MiroProperties? {
 
     return MiroProperties(
         board = properties.getProperty("board"),
-        apiKey = properties.getProperty("apiKey")
+        apiKey = properties.getProperty("accessToken")
     );
 }
 
@@ -44,13 +48,32 @@ fun getMiroProperties(): MiroProperties? {
  * Make a call against Miro
  */
 suspend fun callMiro(properties: MiroProperties): Unit = coroutineScope {
-    val httpClient: HttpClient = HttpClient(Apache) {
+    val httpClient = HttpClient(Apache) {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
         ResponseObserver { response -> println("HTTP status : ${response.status.value}") }
     }
-    val response = async { httpClient.get<HttpResponse>("https://google.com") {} }
+    val board: MiroBoard = httpClient.get("https://api.miro.com/v1/boards/${properties.board}") {
+        headers {
+            append(HttpHeaders.Accept, ContentType.Application.Json)
+            append(HttpHeaders.Authorization, "Bearer ${properties.apiKey}")
+        }
+    }
+    println(board);
 }
 
 /**
  * Dataclass that models Miro configuration
  */
 data class MiroProperties(val board: String, val apiKey: String);
+
+/**
+ * Board data from Miro API
+ */
+@Serializable
+data class MiroBoard(val type: String, val name: String);
