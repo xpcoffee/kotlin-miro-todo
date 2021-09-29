056e3c0121b165d2bc0cd3e7fb4e05b7c78164bc
Emerick Bosch <xpc.dev@gmail.com>
2021-09-29 15:01:22 +0200

    Add Miro properties in an ignored file
    This is where the board and API key will live (for now).

--- src/main/kotlin/Todo.kt
+++ src/main/kotlin/Todo.kt
@@ -3,10 +3,54 @@ import io.ktor.client.engine.apache.*
 import io.ktor.client.features.observer.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import kotlinx.coroutines.*
+import java.io.FileInputStream
+import java.util.*
 
-suspend fun main(args: Array<String>) {
+
+/**
+ * Entry point for the app
+ */
+fun main() = runBlocking {
+    val miroProperties = getMiroProperties();
+    println(miroProperties);
+
+    if(miroProperties != null) {
+        callMiro(miroProperties)
+    }
+}
+
+/**
+ * Read and return Miro configuration
+ */
+fun getMiroProperties(): MiroProperties? {
+    val miroPropertiesFile = "miro.properties"
+    val propertiesPath = object {}.javaClass.getResource(miroPropertiesFile)?.path;
+    if(propertiesPath == null) {
+        println("Unable to find: $miroPropertiesFile")
+        return null;
+    }
+
+    val properties = Properties()
+    properties.load(FileInputStream(propertiesPath))
+
+    return MiroProperties(
+        board = properties.getProperty("board"),
+        apiKey = properties.getProperty("apiKey")
+    );
+}
+
+/**
+ * Make a call against Miro
+ */
+suspend fun callMiro(properties: MiroProperties): Unit = coroutineScope {
     val httpClient: HttpClient = HttpClient(Apache) {
         ResponseObserver { response -> println("HTTP status : ${response.status.value}") }
     }
-    val response = httpClient.get<HttpResponse>("https://google.com") {};
+    val response = async { httpClient.get<HttpResponse>("https://google.com") {} }
 }
+
+/**
+ * Dataclass that models Miro configuration
+ */
+data class MiroProperties(val board: String, val apiKey: String);
