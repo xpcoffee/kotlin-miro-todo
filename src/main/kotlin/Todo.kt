f05f1829caef70c12834f963547e0584080a3f84
Emerick Bosch <xpc.dev@gmail.com>
2021-09-29 13:48:27 +0200

    Make a call against google.com

--- src/main/kotlin/Todo.kt
+++ src/main/kotlin/Todo.kt
@@ -1,3 +1,12 @@
-fun main(args: Array<String>) {
-    println("Hoi, wereld!")
+import io.ktor.client.*;
+import io.ktor.client.engine.apache.*
+import io.ktor.client.features.observer.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
+suspend fun main(args: Array<String>) {
+    val httpClient: HttpClient = HttpClient(Apache) {
+        ResponseObserver { response -> println("HTTP status : ${response.status.value}") }
+    }
+    val response = httpClient.get<HttpResponse>("https://google.com") {};
 }
