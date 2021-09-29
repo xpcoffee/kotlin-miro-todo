b20ea30b516c0ac9e5c58953448ded2c247b7989
Emerick Bosch <xpc.dev@gmail.com>
2021-09-29 17:11:00 +0200

    Refactor main function code

--- src/main/kotlin/Todo.kt
+++ src/main/kotlin/Todo.kt
@@ -8,21 +8,43 @@ fun main() = runBlocking {
 
     if (miroProperties != null) {
         val client = MiroClient(miroProperties)
+
+        /**
+         * Get the frames containing the TODO stickers
+         */
         val frames = client.describeFrames()
         val todoFrames = frames.filter { frame -> miroProperties.frames.indexOf(frame.title) > -1 }
 
+        /**
+         * Get all stickers in the board
+         */
         val stickers = client.describeStickers()
         val stickerMap = stickers.map { it.id to it.text }.toMap();
 
-
-        todoFrames.forEach { frame ->
-            println("---------------")
-            println(frame.title)
-            transformStickerIdsToStickerContents(frame.children, stickerMap).forEach { sticker -> println(sticker) }
-        }
+        /**
+         * Print the contents of stickers in the TODO frames
+         */
+        printTodoState(todoFrames, stickerMap)
     }
 }
 
-fun transformStickerIdsToStickerContents(ids: List<String>, stickerMap: Map<String,String>): List<String> {
-    return ids.mapNotNull { id -> stickerMap.get(id) }
+/**
+ * Prints the stickers in the TODO frames
+ */
+private fun printTodoState(
+    todoFrames: List<MiroFrame>,
+    stickerMap: Map<String, String>
+) {
+    todoFrames.forEach { frame ->
+        println("---------------")
+        println(frame.title)
+        transformWidgetIdsToStickerContents(frame.children, stickerMap).forEach { sticker -> println(sticker) }
+    }
+}
+
+/**
+ * Returns the contents of stickers for a given list of widget ids and a map of stickers.
+ */
+fun transformWidgetIdsToStickerContents(widgetIds: List<String>, stickerMap: Map<String,String>): List<String> {
+    return widgetIds.mapNotNull { id -> stickerMap.get(id) }
 }
